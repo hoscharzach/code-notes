5cd880195b67d8da7514c14cafc7cd7cdc551aba
Zach <hoschar.zach@gmail.com>
2022-12-03 23:20:49 -0600

    text transpiling

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,46 @@
+import { useEffect, useRef, useState } from 'react'
 import './App.css'
+import * as esbuild from 'esbuild-wasm'
 
 function App() {
 
+  const serviceRef = useRef<any>()
+  const [input, setInput] = useState('')
+  const [code, setCode] = useState('')
+
+  const startService = async () => {
+    serviceRef.current = await esbuild.startService({
+      worker: true,
+      wasmURL: '/esbuild.wasm'
+    })
+  }
+
+  useEffect(() => {
+    startService()
+  }, [])
+
+
+
+  const onClick = async () => {
+    if (!serviceRef) {
+      return
+    }
+
+    const result = await serviceRef.current.transform(input, {
+      loader: 'jsx',
+      target: 'es2015',
+    })
+
+    setCode(result.code)
+  }
+
   return (
     <>
-      <h1>Hello From App</h1>
+      <textarea onChange={(e) => setInput(e.target.value)} value={input}></textarea>
+      <div>
+        <button onClick={onClick}>Submit</button>
+      </div>
+      <pre>{code}</pre>
     </>
   )
 }
